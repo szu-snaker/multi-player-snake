f7154342827d116a0cdc205ee18742cd81e4cd51
eternalnext <1059472636@qq.com>
2022-03-05 15:53:54 +0800

    添加后端部署

---
+++ backend/src/main/java/com/example/tanchishe/TanchisheApplication.java
@@ -0,0 +1,13 @@
+package com.example.tanchishe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanchisheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanchisheApplication.class, args);
+    }
+
+}
